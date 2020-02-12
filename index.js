8b1763e9549d1ff63ecddaa7dc2fed900c20d7ad
Yohana Tunarrosa <nytunarr@co.ibm.com>
2020-02-12 12:57:42 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express(),
+path = require('path'),
+cors = require('cors'),
+bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+// UI
+var AssistantV1 = require('watson-developer-cloud/assistant/v1');
+
+//Watson Assistant
+var watsonAssistant = new AssistantV1({
+  iam_apikey: '',
+  version: '2018-02-16',
+  url: ""
+});
+
+var context2 = {};
+
+app.post('/listen', (req, respuesta) => {
+    watsonAssistant.message(
+      {
+        input: { text: req.body.message },
+        context: context2,
+        workspace_id: ''
+      },
+      function (err, response) {
+        if (err) {
+        } else {    
+            var text = {
+            "text": response.output.text[0]
+          }
+          respuesta.send({ success: true, text });
+        }
+      }
+    );
+  });
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
+
+app.use(express.static(path.resolve(__dirname, 'ui/build')));
+
